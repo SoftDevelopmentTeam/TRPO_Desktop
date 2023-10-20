860b94abae32583832d83ed9fa5539829d23fde3
doctorzoiberg <129968489+doctorzoiberg@users.noreply.github.com>
2023-10-20 14:42:36 +0300

    Update download.py

--- cloud/download.py
+++ cloud/download.py
@@ -1,23 +1,29 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+import os
 
 # Авторизация
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth() # Откроет окно браузера для авторизации
+gauth.LocalWebserverAuth()
 
 # Создаем объект GoogleDrive
 drive = GoogleDrive(gauth)
 
-# Создаем файл в корневой папке
-#file1 = drive.CreateFile({'title': 'Пример.txt'})
-#file1.Upload()
+# Указываем путь к папке, куда будут скачиваться файлы
+local_folder_path = 'FileDownload'
+drive_folder_name = 'root'
 
-#print('Файл создан с id:', file1.get('id'))
-
-# Получаем список файлов
+# Получаем список файлов в корневой папке Google Диска
 file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
 
-# Выводим информацию о файлах
-print("Список файлов:")
+# Создаем папку для загрузки файлов
+os.makedirs(local_folder_path, exist_ok=True)
+
+# Скачиваем файлы
 for file in file_list:
-    print('Название: %s, ID: %s' % (file['title'], file['id']))
+    if file['title'] == drive_folder_name:
+        continue  # Пропускаем папку с тем же именем
+    local_file_path = os.path.join(local_folder_path, file['title'])
+    file.GetContentFile(local_file_path)
+
+print(f'Скачано {len(file_list)} файлов в {local_folder_path}.')
