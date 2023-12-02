a5607a5544693ed77683249a21384442bcaa1b18
unknown <mixeew.roman2000@mail.ru>
2023-12-02 17:56:18 +0300

    test

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.12.0-windowsservercore-ltsc2022
+FROM python:3.9-bullseye
 
 ADD requirements.txt .
 RUN pip install -r requirements.txt
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+printf "\n\n%s\n" "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+printf "%-59s" "+ Building program "; printf "%s\n" "+"
+printf "\n\n%s\n" "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
 docker-compose -f ../docker/docker-compose.build.yml up
 cp ../app/client_secrets.json ../dist/
