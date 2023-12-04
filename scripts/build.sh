58f1f99699d7b91d281a7dbe20ca1a395ff5c04a
rmicheev <rmicheev@rmicheev.local>
2023-12-04 09:47:54 +0300

    test

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 printf "\n\n%s\n" "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 printf "%-59s" "+ Building program "; printf "%s\n" "+"
-printf "\n\n%s\n" "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+printf "%s\n\n\n" "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
 docker-compose -f ../docker/docker-compose.build.yml up
 cp ../app/client_secrets.json ../dist/
