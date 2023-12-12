2a9a6eac1b42816df94acd2e111654f28e144dda
RiMikheev <122991783+RiMikheev@users.noreply.github.com>
2023-12-12 14:44:14 +0300

    Parser tests (#32)
    * test
    
    * fixed flake errors
    
    * fixed flake errors
    
    * fixed tests
    
    * Delete docker-compose.yml
    
    * Delete app/cloud/__pycache__ directory
    
    * test
    
    * fixed flake errors
    
    * test
    
    * test
    
    * Delete scripts/build.bat
    
    * test
    
    * test build windows
    
    * build on windows
    
    * build on windows
    
    * test
    
    * test
    
    * Delete scripts/build.bat
    
    * update readme
    
    * Update README.md
    
    * test
    
    * added parser tests
    
    ---------
    
    Co-authored-by: rmicheev <rmicheev@rmicheev.local>
    Co-authored-by: unknown <mixeew.roman2000@mail.ru>

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -18,3 +18,21 @@ def test_parse_text_data(url):
     headers = parser.parse_text(url)
     assert type(headers) == list
     assert len(headers) != 0
+
+
+@pytest.mark.parametrize('url', [('https://www.antutu.com/en/ranking/rank1.htm'),
+                                 ('https://www.antutu.com/en/ranking/ios1.htm'),
+                                 ('https://www.antutu.com/en/ranking/ai1.htm')])
+def test_get_column_name_list(url):
+    headers = parser.parse_text(url)
+    col_name_list = parser.get_column_name_list(headers[1])
+    assert col_name_list[0] == '№'
+
+
+@pytest.mark.parametrize('url, platform', [('https://www.antutu.com/en/ranking/rank1.htm', 'Android'),
+                                 ('https://www.antutu.com/en/ranking/ios1.htm', 'IOS'),
+                                 ('https://www.antutu.com/en/ranking/ai1.htm', 'AI')])
+def test_get_data(url, platform):
+    headers = parser.parse_text(url)
+    data = parser.get_data(headers, platform)
+    assert len(data) != 0
